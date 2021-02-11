bab3086ca0b634f2ef72846f26e8e4ed1c55ec95
Michael Antone <76892533+m-antone1@users.noreply.github.com>
2021-02-10 17:04:12 -0700

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const trLetters = transpose(letters);
+    const verticalJoin = trLetters.map(cl => cl.join(''));
+    let result = false;
+    for (const l of horizontalJoin) {
+      if (l.includes(word)) {
+        result = true;
+      }
     }
-}
+    for (const c of verticalJoin) {
+      if (c.includes(word)) {
+        result = true;
+      }
+    }
+   
+    return result;
+  };
 
 module.exports = wordSearch
